17666a2714fd8bb0e79d61807b7f4600713afcb3
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-06-24 00:20:18 -0300

    Add createGrant, getAuthorizationUrl and handleAuthorizationResponse methods to GithubAuthenticator

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,9 +1,20 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/services.dart';
 import 'package:oauth2/oauth2.dart';
+import 'package:user_repo/auth/domain/auth_failure.dart';
 import 'package:user_repo/auth/infrastructure/credentials_storage/credentials_storage.dart';
 
 class GithubAuthenticator {
   final CredentialsStorage _storage;
+
+  static final authorizationEndpoint =
+      Uri.parse('https://github.com/login/oauth/authorize');
+  static final tokenEndpoint =
+      Uri.parse('https://github.com/login/oauth/access_token');
+  static final redirectUrl = Uri.parse('http://localhost:3000/callback');
+  static const clientId = '4ef5c6c9802322402342';
+  static const clientSecret = '91803395c711651d3529e260b16b4a7865c1dc37';
+  static const scopes = ['read:user', 'repo'];
 
   GithubAuthenticator({
     required CredentialsStorage storage,
@@ -26,4 +37,34 @@ class GithubAuthenticator {
 
   Future<bool> get isSignedIn =>
       getSignedInCredentials().then((credentials) => credentials != null);
+
+  AuthorizationCodeGrant createGrant() {
+    return AuthorizationCodeGrant(
+      clientId,
+      authorizationEndpoint,
+      tokenEndpoint,
+      secret: clientSecret,
+    );
+  }
+
+  Uri getAuthorizationUrl(AuthorizationCodeGrant grant) {
+    return grant.getAuthorizationUrl(redirectUrl, scopes: scopes);
+  }
+
+  Future<Either<AuthFailure, Unit>> handleAuthorizationResponse(
+    AuthorizationCodeGrant grant,
+    Map<String, String> params,
+  ) async {
+    try {
+      final httpClient = await grant.handleAuthorizationResponse(params);
+      await _storage.save(httpClient.credentials);
+      return right(unit);
+    } on FormatException {
+      return left(const AuthFailure.server());
+    } on AuthorizationException catch (error) {
+      return left(AuthFailure.server('${error.error}: ${error.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
 }
