8c862c089a1257278be1e76424ad44a2d50553e8
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-07-05 20:00:08 -0300

    Add AuthState

---
+++ lib/auth/application/auth_notifier.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:user_repo/auth/domain/auth_failure.dart';
+
+part 'auth_notifier.freezed.dart';
+
+@freezed
+class AuthState with _$AuthState {
+  const factory AuthState.initial() = _Initial;
+  const factory AuthState.unauthenticated() = _Unauthenticated;
+  const factory AuthState.authenticated() = _Authenticated;
+  const factory AuthState.failure(AuthFailure failure) = _Failure;
+}
