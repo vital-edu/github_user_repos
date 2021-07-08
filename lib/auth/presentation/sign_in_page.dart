4b6bf0dc5f97ba1f87eece1a9c61323a44eccbeb
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-07-07 23:12:46 -0300

    Add components to SignInPage

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Icon(MdiIcons.github, size: 150),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Welcome to Repo Viewer',
+                    style: Theme.of(context).textTheme.headline3,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Sign In'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
