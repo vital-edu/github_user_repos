0cd39323b1735421f50cc4e861a8edf9db062899
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-07-07 22:55:10 -0300

    Add ios build configuration

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,5 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:user_repo/core/presentation/app_widget.dart';
 
 void main() => runApp(ProviderScope(child: AppWidget()));
+
+// TODO: Replace Flutter Secure Storage with MacOS implementation.
