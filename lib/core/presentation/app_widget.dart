c2830a44c46f9188a1b7a22c4fa53e43265c0c8e
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-07-07 22:55:48 -0300

    Listen to auth state change in app widget

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:user_repo/auth/application/auth_notifier.dart';
+import 'package:user_repo/auth/shared/providers.dart';
 import 'package:user_repo/core/presentation/router/app_router.gr.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final notifier = ref.read(authNotifierProvider.notifier);
+  await notifier.checkAndUpdateAuthStatus();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
   final router = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(initializationProvider, (_) {});
+    ref.listen<AuthState>(authNotifierProvider, (state) {
+      state.maybeMap(
+        orElse: () {},
+        authenticated: (_) => router.pushAndPopUntil(
+          const StarredReposRoute(),
+          predicate: (_) => false,
+        ),
+        unauthenticated: (_) => router.pushAndPopUntil(
+          const SignInRoute(),
+          predicate: (_) => false,
+        ),
+      );
+    });
+
     return MaterialApp.router(
       title: 'Repo Viewer',
       routeInformationParser: router.defaultRouteParser(),
