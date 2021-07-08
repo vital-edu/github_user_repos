720c0d73cd63fc807fa05439bc795ee872e03482
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-07-07 23:21:57 -0300

    Create authorization page and route

--- lib/core/presentation/router/app_router.dart
+++ lib/core/presentation/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:user_repo/auth/presentation/authorization_page.dart';
 import 'package:user_repo/auth/presentation/sign_in_page.dart';
 import 'package:user_repo/splash/presentation/splash_page.dart';
 import 'package:user_repo/starred_repos/presentation/starred_repos_page.dart';
@@ -7,7 +8,8 @@ import 'package:user_repo/starred_repos/presentation/starred_repos_page.dart';
   routes: [
     MaterialRoute<SplashPage>(page: SplashPage, initial: true),
     MaterialRoute<StarredReposPage>(page: StarredReposPage, path: '/starred'),
-    MaterialRoute<SignInPage>(page: SignInPage, path: '/signin')
+    MaterialRoute<SignInPage>(page: SignInPage, path: '/signin'),
+    MaterialRoute<AuthorizationPage>(page: AuthorizationPage, path: '/auth'),
   ],
   replaceInRouteName: 'Page,Route',
 )
