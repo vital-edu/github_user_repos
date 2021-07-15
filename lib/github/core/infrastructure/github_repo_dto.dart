01eb92c3e7e601cb0c6bf9d8bd2547859ee17062
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-07-14 23:20:11 -0300

    Add GitHubRepoDTO

---
+++ lib/github/core/infrastructure/github_repo_dto.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:user_repo/github/core/infrastructure/user_dto.dart';
+
+part 'github_repo_dto.freezed.dart';
+part 'github_repo_dto.g.dart';
+
+String _descriptionFromJson(Object? json) => (json as String?) ?? '';
+
+@freezed
+class GithubRepoDTO with _$GithubRepoDTO {
+  const factory GithubRepoDTO({
+    required UserDTO owner,
+    required String name,
+    @JsonKey(fromJson: _descriptionFromJson) required String description,
+    @JsonKey(name: 'stargazers_count') required int stargazersCount,
+  }) = _GithubRepoDTO;
+
+  factory GithubRepoDTO.fromJson(Map<String, dynamic> json) =>
+      _$GithubRepoDTOFromJson(json);
+}
