d0e7f53098cce901ce9e5aff5d359b184bc316f1
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-07-20 21:42:36 -0300

    Add PaginationLink to GithubHeaders

--- lib/core/infrastructure/github_headers.dart
+++ lib/core/infrastructure/github_headers.dart
@@ -2,14 +2,58 @@ import 'package:dio/dio.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'github_headers.freezed.dart';
+part 'github_headers.g.dart';
 
 @freezed
 class GithubHeaders with _$GithubHeaders {
   const factory GithubHeaders({
     String? etag,
+    PaginationLink? link,
   }) = _GithubHeaders;
 
   factory GithubHeaders.parse(Response response) {
-    return GithubHeaders(etag: response.headers.value('etag'));
+    final link = response.headers.value('link');
+    return GithubHeaders(
+      etag: response.headers.value('etag'),
+      link: link == null
+          ? null
+          : PaginationLink.parse(
+              link.split(','),
+              requestUrl: response.requestOptions.uri.toString(),
+            ),
+    );
   }
+
+  factory GithubHeaders.fromJson(Map<String, dynamic> json) =>
+      _$GithubHeadersFromJson(json);
+}
+
+@freezed
+class PaginationLink with _$PaginationLink {
+  const factory PaginationLink({
+    required int maxPage,
+  }) = _PaginationLink;
+
+  factory PaginationLink.parse(
+    List<String> values, {
+    required String requestUrl,
+  }) =>
+      PaginationLink(
+        maxPage: _extractPageNumber(
+          values.firstWhere(
+            (element) => element.contains('rel="last"'),
+            orElse: () => requestUrl,
+          ),
+        ),
+      );
+
+  static int _extractPageNumber(String value) {
+    final lastPageRegex = RegExp(r'page=(\d+)', caseSensitive: false);
+    final page = lastPageRegex.firstMatch(value)?.group(1);
+
+    return int.tryParse(page.toString())!;
+  }
+
+  factory PaginationLink.fromJson(Map<String, dynamic> json) =>
+      _$PaginationLinkFromJson(json);
 }
