bb0ddd7693e8a1feb78dc4dc761db797a9d2052d
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-11-22 22:18:58 -0300

    Implement getStarredRepoPage and RestApiException

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,14 +1,59 @@
 import 'package:dio/dio.dart';
+import 'package:user_repo/core/infrastructure/dio_extensions.dart';
+import 'package:user_repo/core/infrastructure/network_exceptions.dart';
 import 'package:user_repo/core/infrastructure/remote_response.dart';
+import 'package:user_repo/github/core/infrastructure/github_headers.dart';
+import 'package:user_repo/github/core/infrastructure/github_headers_cache.dart';
 import 'package:user_repo/github/core/infrastructure/github_repo_dto.dart';
 
 class StarredReposRemoteService {
   final Dio _dio;
+  final GithubHeadersCache _headersCache;
 
-  StarredReposRemoteService(this._dio);
+  StarredReposRemoteService(this._dio, this._headersCache);
 
   Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
       int page) async {
-    return const RemoteResponse.noConnection();
+    const token = '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';
+    const acceptHeader = 'application/vnd.github.v3.html+json';
+    final requestUri =
+        Uri.https('api.github.com', '/user/starred', <String, String>{
+      'page': '$page',
+    });
+
+    final previousHeaders = await _headersCache.getHeaders(requestUri);
+
+    try {
+      final response = await _dio.getUri<List<dynamic>>(
+        requestUri,
+        options: Options(headers: <String, String>{
+          'authorization': 'Bearer $token',
+          'accept': acceptHeader,
+          'If-None-Match': previousHeaders?.etag ?? '',
+        }),
+      );
+
+      if (response.statusCode == 304) {
+        return const RemoteResponse.notModified();
+      } else if (response.statusCode != 200) {
+        throw RestApiException(response.statusCode);
+      }
+
+      final headers = GithubHeaders.parse(response);
+      await _headersCache.saveHeaders(requestUri, headers);
+      final data = response.data ?? <Map<String, Object>>[];
+      final convertedData = data
+          .map((dynamic e) => GithubRepoDTO.fromJson(e as Map<String, dynamic>))
+          .toList();
+      return RemoteResponse.withNewData(convertedData);
+    } on DioError catch (error) {
+      if (error.isConnectionError) {
+        return const RemoteResponse.noConnection();
+      } else if (error.response != null) {
+        throw RestApiException(error.response!.statusCode);
+      }
+
+      rethrow;
+    }
   }
 }
