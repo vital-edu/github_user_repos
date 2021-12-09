0cf9ac1f8da6365b0365c3928a411c4ed430daf9
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-12-08 21:50:11 -0300

    Implement load new content when scrolling StarredReposListView

--- lib/github/repos/starred_repos/presentation/paginated_repos_list_view.dart
+++ lib/github/repos/starred_repos/presentation/paginated_repos_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:user_repo/github/repos/starred_repos/application/starred_repos_notifier.dart';
 import 'package:user_repo/github/repos/starred_repos/presentation/failure_repo_tile.dart';
 import 'package:user_repo/github/repos/starred_repos/presentation/loading_repo_tile.dart';
 import 'package:user_repo/github/repos/starred_repos/presentation/repo_tile.dart';
@@ -17,6 +18,8 @@ class PaginatedReposListView extends ConsumerStatefulWidget {
 
 class _PaginatedReposListViewState
     extends ConsumerState<PaginatedReposListView> {
+  bool canLoadNextPage = true;
+
   @override
   void initState() {
     super.initState();
@@ -27,6 +30,47 @@ class _PaginatedReposListViewState
   Widget build(BuildContext context) {
     final state = ref.watch(starredReposNotifierProvider);
 
+    ref.listen<StarredReposState>(starredReposNotifierProvider,
+        (previous, next) {
+      next.map(
+        initial: (_) => canLoadNextPage = true,
+        loadInProgress: (_) => canLoadNextPage = false,
+        loadSuccess: (_) => canLoadNextPage = _.isNextPageAvailable,
+        hasFailure: (_) => canLoadNextPage = false,
+      );
+    });
+
+    return NotificationListener<ScrollNotification>(
+      child: _PaginatedReposListView(state: state),
+      onNotification: (notification) {
+        final pixels = notification.metrics.pixels;
+        final viewportDimension = notification.metrics.viewportDimension;
+        final maxScrollExtent = notification.metrics.maxScrollExtent;
+
+        final limit = maxScrollExtent - viewportDimension / 3;
+
+        if (canLoadNextPage && pixels >= limit) {
+          canLoadNextPage = false;
+          ref
+              .read(starredReposNotifierProvider.notifier)
+              .getNextStarredReposPage();
+        }
+        return false;
+      },
+    );
+  }
+}
+
+class _PaginatedReposListView extends StatelessWidget {
+  const _PaginatedReposListView({
+    Key? key,
+    required this.state,
+  }) : super(key: key);
+
+  final StarredReposState state;
+
+  @override
+  Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return state.map(
